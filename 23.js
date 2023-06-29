458ebc7b03db249e57847b47aac516cf94e77af7
svenschilling <sven.schilling@gmail.com>
2023-06-29 15:35:53 +0200

    upload

---
+++ 23.js
@@ -0,0 +1,18 @@
+SCREEN_WIDTH = 500;
+SCREEN_HEIGHT = 500;
+
+function setup() {
+    createCanvas(SCENE_WIDTH, SCENE_HEIGHT);
+    noStroke();
+    
+
+}
+
+function draw() {
+    
+}
+
+function drawShape(x, y, innerShapes ) {
+    
+}
+
